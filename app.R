92967fdc5a449fc53aaf427e4b38db5b86bc822e
Ed Baker <edwbaker@gmail.com>
2022-07-30 12:23:18 +0100

    Add citation.

--- app.R
+++ app.R
@@ -2,79 +2,115 @@ library(shiny)
 library(suncalc)
 library(plotrix)
 library(sonicscrewdriver)
+library(schite)
 
 year <- as.POSIXlt(Sys.Date())$year + 1900
 
 ui <- fluidPage(
-    # Application title
-    titlePanel("Daylight Information"),
+  # Application title
+  titlePanel("Daylight Information"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("date",
-                        "Date:",
-                        min = as.Date(paste0(year,"-01-01"),"%Y-%m-%d"),
-                        max = as.Date(paste0(year,"-12-31"),"%Y-%m-%d"),
-                        value=Sys.Date(),
-                        timeFormat="%Y-%m-%d",
-                        step = 3,
-                        animate = animationOptions(interval = 250,loop=TRUE)
-            ),
-            selectInput("loc",
-              "Location:",
-              c("NHM", "York", "Houndwood", "Lyme Regis", "Rovaniemi", "Austin", "Sydney"),
-              selected = "York",
-              multiple = FALSE,
-              selectize = TRUE,
-              width = NULL,
-              size = NULL
-            ),
-            checkboxGroupInput("times",
-              "Time of day:",
-              choices = c(
-                "Sunrise",
-                "Sunset",
-                "Solar Noon",
-                "Civil Twilight",
-                "Nautical Twilight",
-                "Astronomical Twilight",
-                "Night",
-                "Nadir"
-                ),
-              selected = c(
-                "Sunrise",
-                "Sunset",
-                "Civil Twilight",
-                "Nautical Twilight",
-                "Astronomical Twilight",
-                "Night"
-              ),
-            ),
-            selectInput("display",
-                        "Display:",
-                        c("Main", "Core", 'Ring'),
-                        selected = "Main",
-                        multiple = FALSE,
-                        selectize = TRUE,
-                        width = NULL,
-                        size = NULL
-            ),
-            checkboxInput("legend",
-                          "Show legend",
-                          value=FALSE
-            )
-        ),
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("date",
+                  "Date:",
+                  min = as.Date(paste0(year,"-01-01"),"%Y-%m-%d"),
+                  max = as.Date(paste0(year,"-12-31"),"%Y-%m-%d"),
+                  value=Sys.Date(),
+                  timeFormat="%Y-%m-%d",
+                  step = 3,
+                  animate = animationOptions(interval = 250,loop=TRUE)
+      ),
+      selectInput("loc",
+                  "Location:",
+                  c("NHM", "York", "Houndwood", "Lyme Regis", "Rovaniemi", "Austin", "Sydney"),
+                  selected = "York",
+                  multiple = FALSE,
+                  selectize = TRUE,
+                  width = NULL,
+                  size = NULL
+      ),
+      checkboxGroupInput("times",
+                         "Time of day:",
+                         choices = c(
+                           "Sunrise",
+                           "Sunset",
+                           "Solar Noon",
+                           "Civil Twilight",
+                           "Nautical Twilight",
+                           "Astronomical Twilight",
+                           "Night",
+                           "Nadir"
+                         ),
+                         selected = c(
+                           "Sunrise",
+                           "Sunset",
+                           "Civil Twilight",
+                           "Nautical Twilight",
+                           "Astronomical Twilight",
+                           "Night"
+                         ),
+      ),
+      selectInput("display",
+                  "Display:",
+                  c("Main", "Core", 'Ring'),
+                  selected = "Main",
+                  multiple = FALSE,
+                  selectize = TRUE,
+                  width = NULL,
+                  size = NULL
+      ),
+      checkboxInput("legend",
+                    "Show legend",
+                    value=FALSE
+      )
+    ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      tabsetPanel(type = "tabs",
+                  tabPanel("How to cite", htmlOutput("citation")),
+                  tabPanel("Data Calculation", htmlOutput("citecalc")),
+                  tabPanel("Data Visualisation", htmlOutput("citevis")),
+                  tabPanel("Misc", htmlOutput("citmisc"))
+      )
     )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  citation <- list(
+    cite_bibentry(
+      bibentry(
+        bibtype="Misc",
+        title="schite: citations for shiny",
+        author="Ed Baker",
+        url="https://github.com/edwbaker/schite",
+        year=2022
+      )
+    )
+  )
+  output$citation <- citationTabUI(citation)
+
+  citcalc <- list(
+    cite_r_package("suncalc")
+  )
+  output$citecalc <- citationTabUI(citcalc)
+
+  citvis <- list(
+    cite_r_package("sonicscrewdriver"),
+    cite_r_package("plotrix")
+  )
+  output$citevis <- citationTabUI(citvis)
+
+  citmisc <- list(
+    cite_r_package("shiny")
+  )
+  output$citmisc <- citationTabUI(citmisc)
+
   output$distPlot <- renderPlot({
     if (input$loc == "NHM") {
       lat = 51.49
