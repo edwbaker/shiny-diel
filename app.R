84b06ae47dcd91722a5352f348613160d01ee6b9
Ed Baker <edwbaker@gmail.com>
2023-01-09 22:39:49 +0000

    Coding standards.

--- app.R
+++ app.R
@@ -17,14 +17,15 @@ ui <- fluidPage(
                   "Date:",
                   min = as.Date(paste0(year,"-01-01"),"%Y-%m-%d"),
                   max = as.Date(paste0(year,"-12-31"),"%Y-%m-%d"),
-                  value=Sys.Date(),
-                  timeFormat="%Y-%m-%d",
+                  value = Sys.Date(),
+                  timeFormat = "%Y-%m-%d",
                   step = 3,
                   animate = animationOptions(interval = 250,loop=TRUE)
       ),
       selectInput("loc",
                   "Location:",
-                  c("NHM", "York", "Houndwood", "Lyme Regis", "Rovaniemi", "Austin", "Sydney"),
+                  c("NHM", "York", "Houndwood", "Lyme Regis",
+                  "Rovaniemi", "Austin", "Sydney"),
                   selected = "York",
                   multiple = FALSE,
                   selectize = TRUE,
@@ -54,7 +55,7 @@ ui <- fluidPage(
       ),
       selectInput("display",
                   "Display:",
-                  c("Main", "Core", 'Ring'),
+                  c("Main", "Core", "Ring"),
                   selected = "Main",
                   multiple = FALSE,
                   selectize = TRUE,
@@ -63,7 +64,7 @@ ui <- fluidPage(
       ),
       checkboxInput("legend",
                     "Show legend",
-                    value=FALSE
+                    value = FALSE
       )
     ),
 
@@ -85,11 +86,11 @@ server <- function(input, output, session) {
   citation <- list(
     cite_bibentry(
       bibentry(
-        bibtype="Misc",
-        title="Daylight Information Example",
-        author="Ed Baker",
-        url="http://shiny.ebaker.me.uk/shiny-diel/",
-        year=2022
+        bibtype = "Misc",
+        title = "Daylight Information Example",
+        author = "Ed Baker",
+        url = "http://shiny.ebaker.me.uk/shiny-diel/",
+        year = 2022
       )
     )
   )
@@ -114,48 +115,54 @@ server <- function(input, output, session) {
 
   output$distPlot <- renderPlot({
     if (input$loc == "NHM") {
-      lat = 51.49
-      lon = -0.17
+      lat <- 51.49
+      lon <- -0.17
     }
     if (input$loc == "York") {
-      lat = 53.95
-      lon = -1.10
+      lat <- 53.95
+      lon <- -1.10
     }
     if (input$loc == "Houndwood") {
-      lat = 55.87
-      lon = -2.25
+      lat <- 55.87
+      lon <- -2.25
     }
     if (input$loc == "Lyme Regis") {
-      lat = 50.725
-      lon = -2.940
+      lat <- 50.725
+      lon <- -2.940
     }
     if (input$loc == "Rovaniemi") {
-      lat = 66.5
-      lon = 25.733333
+      lat <- 66.5
+      lon <- 25.733333
     }
     if (input$loc == "Austin") {
-      lat = 30.267222
-      lon = -97.743056
+      lat <- 30.267222
+      lon <- -97.743056
     }
     if (input$loc == "Sydney") {
-      lat = -33.867778
-      lon = 151.21
+      lat <- -33.867778
+      lon <- 151.21
     }
 
     if (input$display == "Main") {
-      inner = 0
-      outer = 2
+      inner <- 0
+      outer <- 2
     }
     if (input$display == "Core") {
-      inner = 0
-      outer = 1
+      inner <- 0
+      outer <- 1
     }
     if (input$display == "Ring") {
-      inner = 1.75
-      outer = 2
+      inner <- 1.75
+      outer <- 2
     }
-    dielPlot(as.POSIXct(input$date),lat,lon,c(inner,outer),input$times,legend=input$legend)
+    dielPlot(as.POSIXct(input$date),
+    lat,
+    lon,
+    c(inner, outer),
+    input$times,
+    legend=input$legend)
   })
+
 }
 
 # Run the application
